cdd6b9f31f617eaf5e28f50907b95b44f85dfd44
nicola <nicolawongwaiyee@gmail.com>
2018-08-09 17:42:49 +0200

    minor update

--- test/ci.sh
+++ test/ci.sh
@@ -3,6 +3,6 @@
 python repeatcraft.py -r example/example_input.gff -u example/example_input.out -c example/repeatcraft.cfg -o example_strict -m strict
 
 # loose mode
-../repeatcraft.py -r ../example/example_input.gff -u ../example/example_input.out -c ../example/repeatcraft.cfg -o example_loose -m loose
+python repeatcraft.py -r example/example_input.gff -u example/example_input.out -c example/repeatcraft.cfg -o example_loose -m loose
 
 
